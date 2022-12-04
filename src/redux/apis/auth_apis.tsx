6f15a768ce7c5b43b7edd94548ca31678e411799
Devan Kavalchek <devankav7@gmail.com>
2022-12-04 15:16:22 -0500

    Added login page and api

--- src/redux/apis/auth_apis.tsx
+++ src/redux/apis/auth_apis.tsx
@@ -16,3 +16,20 @@ export const registerAccountAPI = ({
     .error(400, (error) => error.message)
     .error(409, (error) => error.message)
     .json();
+
+export const loginAPI = ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) =>
+  wretch('/api/auth/login')
+    .errorType('json')
+    .post({
+      email,
+      password,
+    })
+    .error(400, (error) => error.message)
+    .error(409, (error) => error.message)
+    .json();
