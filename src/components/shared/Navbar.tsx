0e7905b72d0244d0eca98fcd80a4cb5197495438
Devan Kavalchek <devankav7@gmail.com>
2023-02-15 13:07:23 -0500

    Updated prettierrc

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,15 +3,15 @@ import froggyLogo from 'assets/images/froggy-logo.png';
 
 const Navbar = () => {
   return (
-    <Container className='px-lg pt-md mb-md'>
-      <Row alignItems='center' justifyContent='space-between' className='mx-lg'>
-        <img src={froggyLogo} className='navbar-logo mr-xs' />
+    <Container className="px-lg pt-md mb-md">
+      <Row alignItems="center" justifyContent="space-between" className="mx-lg">
+        <img src={froggyLogo} className="navbar-logo mr-xs" />
         <h1>froggy</h1>
-        <Row justifyContent='end' alignItems='center'>
-          <a href='/register' className='pr-md'>
+        <Row justifyContent="end" alignItems="center">
+          <a href="/register" className="pr-md">
             Register
           </a>
-          <a href='/login'>Login</a>
+          <a href="/login">Login</a>
         </Row>
       </Row>
     </Container>
